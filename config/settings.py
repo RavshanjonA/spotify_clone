19a01870eca5f6769107fb3ef42fd2079cef4f1c
ahmadjonov-98 <akhmadjonovr.98@gmail.com>
2024-03-27 18:14:56 +0500

    feat: send activation link added

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "137.184.40.10"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 AUTH_USER_MODEL = "user.User"
 
